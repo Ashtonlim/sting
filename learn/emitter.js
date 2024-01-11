27d74391dba1f1c214da25bb2e182563925d791f
ashtonlim <ashtonlim22@gmail.com>
2024-01-11 14:34:40 +0800

    emit

--- learn/emitter.js
+++ learn/emitter.js
@@ -5,5 +5,15 @@ function Emitter() {
 Emitter.prototype.on = function (type, listener) {
   // if this.events[type] is undefined, set it to an empty array
   this.events[type] = this.events[type] || [];
+  this.events[type].push(listener);
 };
+
+Emitter.prototype.emit = function (type) {
+  if (this.events[type]) {
+    this.events[type].forEach((listener) => {
+      listener();
+    });
+  }
+};
+
 console.log();
