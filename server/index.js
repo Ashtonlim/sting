bfe32689304b8412923ad2a6c65b3e6bc4d2272f
Ashton Lim <ashtonlim22@gmail.com>
2024-01-11 14:48:59 +0800

    Dev (#3)
    * new msg
    
    * added learning module
    
    * updated learning mod
    
    * updated learning mod
    
    * emit
    
    * added server and gitignore
    
    * added server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,9 @@
-console.log('hi')
-console.log('added new')
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true })); // Setup the body parser to handle form submits
+app.use(session({ secret: "super-secret" })); // Session setup
