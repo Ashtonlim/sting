6e4541c51cf3672f73cbce50fd488244e9e02e37
ashtonlim <ashtonlim22@gmail.com>
2024-01-24 08:22:11 +0800

    m.

--- init.sql
+++ init.sql
@@ -45,8 +45,43 @@ insert into employees (id, name, salary, managerId) values
 
 use testdb
 
+select * FROM first_table
+
+insert into first_table values 
+(1, 'test', 'text', NOW()), (2, 'test2', 'text', NOW()), (3, 'test3', 'text', NOW()), 
+(4, 'test1', 'text4', NOW()), (5, 'test22', 'text3', NOW()), (6, 'test3', 'text3', NOW())
+
+delete * from first_table left join (SELECT MIN(ft_id) AS min_ft_id
+    FROM first_table
+    GROUP BY my_text) as min_ids on first_table.ft_id = min_ids.min_ft_id where min_ft_id is null
+
+Select * from first_table where ft_id not in (select A.* from (select min(ft_id) from first_table group by my_text) as A
+)
+
+select A.* from (select min(ft_id) from first_table group by my_text) as A
+
+select p1.* from first_table p1, first_table p2 where p1.my_text=p2.my_text and p1.ft_id < p2.ft_id
+
+Truncate first_table
+
+DELETE first_table.*
+FROM first_table
+LEFT JOIN (
+    SELECT MIN(ft_id) AS min_ft_id
+    FROM first_table
+    GROUP BY my_text
+) AS min_ids
+ON first_table.ft_id = min_ids.min_ft_id
+WHERE min_ids.min_ft_id IS NULL;
+
+
+
+select min(ft_id) from first_table group by my_text
+
 Select e1.name from employees e1 join employees e2 on e1.managerId = e2.id where e1.salary > e2.salary
 
 
 use sakila
+
+select * from customer having count(first_name) > 1
 
