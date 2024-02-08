b79a5a1a3755f627ff63cf369c75e79508598f50
ashtonlim <ashtonlim22@gmail.com>
2024-02-08 09:34:45 +0800

    simplified tag grp

--- client/src/pages/Dashboard/TagGroup.jsx
+++ client/src/pages/Dashboard/TagGroup.jsx
@@ -1,50 +1,12 @@
-import { useEffect, useRef, useState } from "react";
-import { PlusOutlined } from "@ant-design/icons";
-import { TweenOneGroup } from "rc-tween-one";
-import { Input, Tag, theme } from "antd";
+import { useState } from "react";
+import { Tag } from "antd";
 
 const TagGroup = ({ groups = [] }) => {
   // console.log("groups", groups);
-  const { token } = theme.useToken();
   const [tags, setTags] = useState(groups || []);
-  const [inputVisible, setInputVisible] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-  const inputRef = useRef(null);
-  useEffect(() => {
-    if (inputVisible) {
-      inputRef.current?.focus();
-    }
-  }, [inputVisible]);
-  const handleClose = (removedTag) => {
-    const newTags = tags.filter((tag) => tag !== removedTag);
-    console.log(newTags);
-    setTags(newTags);
-  };
-  const showInput = () => {
-    setInputVisible(true);
-  };
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-  const handleInputConfirm = () => {
-    if (inputValue && tags.indexOf(inputValue) === -1) {
-      setTags([...tags, inputValue]);
-    }
-    setInputVisible(false);
-    setInputValue("");
-  };
+
   const forMap = (tag) => {
-    const tagElem = (
-      <Tag
-        closable
-        onClose={(e) => {
-          e.preventDefault();
-          handleClose(tag);
-        }}
-      >
-        {tag}
-      </Tag>
-    );
+    const tagElem = <Tag>{tag}</Tag>;
     return (
       <span
         key={tag}
@@ -57,60 +19,11 @@ const TagGroup = ({ groups = [] }) => {
     );
   };
   const tagChild = tags.map(forMap);
-  const tagPlusStyle = {
-    background: token.colorBgContainer,
-    borderStyle: "dashed",
-  };
+
   return (
     <>
-      <div
-        style={{
-          marginBottom: 16,
-        }}
-      >
-        <TweenOneGroup
-          enter={{
-            scale: 0.8,
-            opacity: 0,
-            type: "from",
-            duration: 100,
-          }}
-          onEnd={(e) => {
-            if (e.type === "appear" || e.type === "enter") {
-              e.target.style = "display: inline-block";
-            }
-          }}
-          leave={{
-            opacity: 0,
-            width: 0,
-            scale: 0,
-            duration: 200,
-          }}
-          appear={false}
-        >
-          {tagChild}
-        </TweenOneGroup>
-      </div>
+      <div className="mb-3">{tagChild}</div>
     </>
   );
 };
 export default TagGroup;
-
-// {inputVisible ? (
-//   <Input
-//     ref={inputRef}
-//     type="text"
-//     size="small"
-//     style={{
-//       width: 78,
-//     }}
-//     value={inputValue}
-//     onChange={handleInputChange}
-//     onBlur={handleInputConfirm}
-//     onPressEnter={handleInputConfirm}
-//   />
-// ) : (
-//   <Tag onClick={showInput} style={tagPlusStyle}>
-//     <PlusOutlined /> New Tag
-//   </Tag>
-// )}
